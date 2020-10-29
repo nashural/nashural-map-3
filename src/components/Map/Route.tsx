0311a50d49af1870e5131e82e56e237abddba5c3
Yury Chebotaev <yury.79120345101@gmail.com>
2020-10-29 18:34:28 +0500

    Move RouteManager to separate file

--- src/components/Map/Route.tsx
+++ src/components/Map/Route.tsx
@@ -1,65 +1,9 @@
 import { FC, memo, useLayoutEffect, useMemo } from 'react'
 import { withYMaps } from 'react-yandex-maps'
 
-import { RouteProps, RouteManagerCache } from './typings.d';
-import { GeoJSONCoordinates } from '../../typings';
+import { RouteManager } from './RouteManager'
 
-class RouteManager {
-  private ymaps: any
-  private mapRef: any
-  private cache: RouteManagerCache = {}
-  private updateLock: boolean = false
-
-  constructor(ymaps: any, mapRef: any) {
-    this.ymaps = ymaps
-    this.mapRef = mapRef
-  }
-
-  get map() {
-    return this.mapRef.current
-  }
-
-  hashPoints(points: GeoJSONCoordinates[]) {
-    let hash: number = points.length ^ 0xffffff
-    for (let [lat, lon] of points) {
-      hash ^= lat
-      hash ^= lon
-    }
-    return hash
-  }
-
-  async getOrCreateRoute(points: GeoJSONCoordinates[]) {
-    const hash = this.hashPoints(points)
-    if (this.cache[hash]) {
-      return this.cache[hash]
-    } else {
-      const route = await this.ymaps.route(points)
-      this.cache[hash] = route
-      return route
-    }
-  }
-
-  async update(points: GeoJSONCoordinates[]) {
-    if (this.updateLock) {
-      return 
-    } else {
-      this.updateLock = true
-      const route = await this.getOrCreateRoute(points)
-      const idx = this.map.geoObjects.indexOf(route)
-      if (idx === -1) {
-        this.map.geoObjects.add(route)
-      }
-      this.updateLock = false
-    }
-  }
-
-  clear() {
-    for (let [hash, route] of Object.entries(this.cache)) {
-      this.map.geoObjects.remove(route)
-      delete this.cache[hash]
-    }
-  }
-}
+import { RouteProps } from './typings.d'
 
 const Route_: FC<RouteProps> = memo<RouteProps>(({ points, mapRef, ymaps }) => {
   const routeManager = useMemo(() => new RouteManager(ymaps, mapRef), [ymaps, mapRef])
