244868ba7b62f4636ac6cb0b0de81f991e1a32b4
Yury Chebotaev <yury.79120345101@gmail.com>
2020-10-30 21:58:03 +0500

    Set page title

--- public/upload/index.php
+++ public/upload/index.php
@@ -129,7 +129,7 @@
 
 <head>
   <meta charset="utf-8">
-  <title></title>
+  <title>Управление маршрутами</title>
   <meta name="description" content="">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
