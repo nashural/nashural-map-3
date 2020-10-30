48f1e5b02ebe70f9563aaad85cb8918da7355939
Yury Chebotaev <yury.79120345101@gmail.com>
2020-10-30 07:56:49 +0500

    Remove commented code

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -12,35 +12,7 @@ import { AppProps } from './typings'
 
 import "./desktop.css"
 
-// DEBUG:
-// import { useDispatch } from '../../hooks/useDispatch'
-// import { useEffect } from 'react'
-// import { toggleDrawer } from '../../store/slices/drawer'
-// import { toggleRouter } from '../../store/slices/router'
-
 export const App: FC<AppProps> = () => {
-  // const dispatch = useDispatch()
-
-  // useEffect(() => {
-  //   let t2: any
-  //   let t3: any
-  //   const t = setTimeout(() => {
-  //     dispatch(toggleDrawer({ on: false }))
-  //     dispatch(toggleRouter({ on: true }))
-  //     t2 = setTimeout(() => {
-  //       dispatch(toggleRouter({ on: false }))
-  //     }, 2 * 1000)
-  //     t3 = setTimeout(() => {
-  //       dispatch(toggleDrawer({ on: true }))
-  //     }, 4 * 1000)
-  //   }, 5 * 1000)
-  //   return () => {
-  //     clearTimeout(t)
-  //     clearTimeout(t2)
-  //     clearTimeout(t3)
-  //   }
-  // }, [dispatch])
-
   const drawerOpened = useSelector(drawerOpenedSelector)
   const routerOpened = useSelector(routerOpenedSelector)
 
