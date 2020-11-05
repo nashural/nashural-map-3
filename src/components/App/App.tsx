accad5231b63e12cbb8f2e7e2926eeea85515206
Yury Chebotaev <yury.79120345101@gmail.com>
2020-11-05 10:20:40 +0500

    Make App mobile aware

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react'
 import { useSelector } from 'react-redux'
+import Media from 'react-media'
 
 import { Groups } from '../Groups'
 import { Header } from '../Header'
@@ -7,21 +8,45 @@ import { Map } from '../Map'
 import { Router } from '../Router'
 import { drawerOpenedSelector } from '../../store/slices/drawer'
 import { routerOpenedSelector } from '../../store/slices/router'
+import { DESKTOP, MOBILE } from '../constants/mediaQueries';
 
 import { AppProps } from './typings.d'
 
+import "./universal.css"
 import "./desktop.css"
+import "./mobile.css"
 
 export const App: FC<AppProps> = () => {
   const drawerOpened = useSelector(drawerOpenedSelector)
   const routerOpened = useSelector(routerOpenedSelector)
 
   return (
-    <div className={`App ${drawerOpened ? 'drawer' : ''} ${routerOpened ? 'router' : ''}`}>
-      <Header />
-      <Groups />
-      <Map />
-      <Router />
-    </div>
+    <Media queries={{ mobile: MOBILE, desktop: DESKTOP }}>{({ mobile, desktop }: { mobile: boolean, desktop: boolean }) => {
+      if (mobile) {
+        return (
+          <div className={`App ${drawerOpened ? 'drawer' : ''} ${routerOpened ? 'router' : ''} mobile`}>
+            <Header />
+            <div className="App-screen mobile">
+              <Groups />
+              <Map />
+              <Router />
+            </div>
+          </div>
+        )
+      }
+
+      if (desktop) {
+        return (
+          <div className={`App ${drawerOpened ? 'drawer' : ''} ${routerOpened ? 'router' : ''} desktop`}>
+            <Header />
+            <Groups />
+            <Map />
+            <Router />
+          </div>
+        )
+      }
+
+      return null
+    }}</Media>
   )
 }
