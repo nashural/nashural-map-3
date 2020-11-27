1c0158b452f78f7b2945d623f7ec62f927bb12a4
Yury Chebotaev <yury.79120345101@gmail.com>
2020-11-27 18:31:29 +0500

    Introduce cypress-image-diff-js to cypress

--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -1,5 +1,7 @@
 const cypressTypeScriptPreprocessor = require('./cy-ts-preprocessor')
 
-module.exports = (on: any) => {
+module.exports = (on: any, config: any) => {
   on('file:preprocessor', cypressTypeScriptPreprocessor)
+
+  require('cypress-image-diff-js/dist/plugin')(on, config)
 }
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,3 +1,6 @@
+// @ts-ignore
+import compareSnapshotCommand from 'cypress-image-diff-js/dist/command'
+
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
@@ -23,3 +26,5 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+compareSnapshotCommand()
